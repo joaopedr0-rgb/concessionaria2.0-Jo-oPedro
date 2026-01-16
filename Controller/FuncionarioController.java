5d5f5cd0e20456a4c884c217065675421ddbb756
joaopedr0-rgb <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>
2026-01-16 16:03:02 -0300

    terceiro commit

---
+++ Controller/FuncionarioController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class FuncionarioController {
+    public class funcionariosModel {
+        ArrayList<String> listafuncionarios = new ArrayList<String>();
+
+        public funcionariosModel() {
+            listafuncionarios.add("João Silva - Gerente - joao.silva@concessionaria.com - 28");
+            listafuncionarios.add("Maria Oliveira - Vendedora - maria.oliveira@concessionaria.com - 25");
+        }
+    }
+}
